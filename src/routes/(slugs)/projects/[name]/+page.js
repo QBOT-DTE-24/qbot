1648c524e4dfe1067a1084cb5a8fe7ad2e2c1d68
y23d17 <mjfumiten@antiquespride.edu.ph>
2026-04-20 04:32:56 +0800

    fix: changing slug to name slug directory

--- src/routes/(slugs)/projects/[name]/+page.js
+++ src/routes/(slugs)/projects/[name]/+page.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 import { items } from '@/types/app.js';
 
 export const load = ({ params }) => {
-	const item = items.find((i) => i.name.toLocaleLowerCase() === params.slug);
+	const item = items.find((i) => i.name.toLocaleLowerCase() === params.name);
 
 	if (!item) {
 		throw error(404, 'Item not found');
